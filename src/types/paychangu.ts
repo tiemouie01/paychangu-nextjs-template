a2f848743a3cefc81e5968743b8c214a7b748bf9
Timothy Miamba <timal0361@gmail.com>
2024-11-27 16:23:25 +0200

    chore: Add level verification types

--- src/types/paychangu.ts
+++ src/types/paychangu.ts
@@ -49,3 +49,52 @@ export type LevelCallbackPayload = {
   created_at: string;
   updated_at: string;
 };
+
+// Types for payload received when verifying a transaction
+export type LevelVerifyAuthorization = {
+  channel: string;
+  card_number: string | null;
+  expiry: string | null;
+  brand: string | null;
+  provider: string | null;
+  mobile_number: string | null;
+  completed_at: string;
+};
+
+export type LevelVerifyCustomer = {
+  email: string;
+  first_name: string;
+  last_name: string;
+};
+
+export type LevelVerifyLog = {
+  type: string;
+  message: string;
+  created_at: string;
+};
+
+export type LevelVerifyData = {
+  event_type: string;
+  tx_ref: string;
+  mode: string;
+  type: string;
+  status: string;
+  number_of_attempts: number;
+  reference: string;
+  currency: "MWK" | "USD";
+  amount: number;
+  charges: number;
+  customization?: LevelCustomization;
+  meta: LevelMeta | null;
+  authorization: LevelVerifyAuthorization;
+  customer?: LevelVerifyCustomer;
+  logs: LevelVerifyLog[];
+  created_at: string;
+  updated_at: string;
+};
+
+export type LevelVerifyPayload = {
+  status: string;
+  message: string;
+  data: LevelVerifyData;
+};
