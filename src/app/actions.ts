624735fdd4416aa8160a548e42aa44969b03133d
Timothy Miamba <timal0361@gmail.com>
2024-11-27 15:21:47 +0200

    feat: Complete checkout implementation

---
+++ src/app/actions.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { formSchema } from "@/lib/form-schemas";
+import { LevelData } from "@/types/paychangu";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+// You can change the type of the data parameter to any type that will match the LevelData
+// type defined in src/types/paychangu.ts
+
+// The inferred type is used here because this implementation used a React Hook Form
+
+export async function checkout(values: z.infer<typeof formSchema>) {
+  // Generate a random transaction reference to be used to identify the transaction.
+  // Store in db to be used to verify the transaction later.
+  const tx_ref = crypto.randomUUID();
+
+  // Clean data to match the Level Config requirements
+  const data: LevelData = {
+    amount: values.amount,
+    currency: values.currency,
+    email: values.email,
+    first_name: values.firstname,
+    last_name: values.lastname,
+    callback_url: values.callbackUrl,
+    return_url: values.returnUrl,
+    tx_ref,
+  };
+
+  // Send a fetch request to the PayChangu API using your secret key and the data
+  const response = await fetch("https://api.paychangu.com/payment", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${process.env.PAYCHANGU_SECRET_KEY}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  // Get the checkout URL from the response and redirect to it
+  const text = await response.text();
+  const checkoutUrl = JSON.parse(text).data.checkout_url;
+
+  redirect(checkoutUrl);
+}
--- src/components/payment-form.tsx
+++ src/components/payment-form.tsx
@@ -24,33 +24,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
-// Form schema definition with zod
-const formSchema = z.object({
-  firstname: z.string().min(2, {
-    message: "Firstname must be at least 2 characters.",
-  }),
-  lastname: z.string().min(2, {
-    message: "lastname must be at least 2 characters.",
-  }),
-  email: z.string().email({
-    message: "Please enter a valid email address.",
-  }),
-  currency: z.enum(["MWK", "USD"], {
-    message: "Please select a valid currency.",
-  }),
-  amount: z.number().int().positive({
-    message: "Please enter a valid amount.",
-  }),
-  callbackUrl: z.string().url({
-    message: "Please enter a valid URL.",
-  }),
-  returnUrl: z.string().url({
-    message: "Please enter a valid URL.",
-  }),
-});
+import { formSchema } from "@/lib/form-schemas";
+import { checkout } from "@/app/actions";
 
 export default function PaymentForm() {
+  // Define the form
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -64,11 +42,9 @@ export default function PaymentForm() {
     },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    // Get the checkout url by making a call to the PayChangu Level API
+    checkout(values);
   }
 
   return (
@@ -223,7 +199,7 @@ export default function PaymentForm() {
           className="md:col-span-2"
           disabled={form.formState.isSubmitting}
         >
-          Get Checkout URL
+          Checkout
         </Button>
       </form>
     </Form>
