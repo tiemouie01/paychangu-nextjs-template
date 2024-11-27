e363a900007202b7b794749feedbf78923ad160f
Timothy Miamba <timal0361@gmail.com>
2024-11-27 15:29:56 +0200

    chore: Add webhook payload types

--- src/types/paychangu.ts
+++ src/types/paychangu.ts
@@ -1,6 +1,7 @@
 export type LevelCustomization = {
-  title: string;
-  description: string;
+  title: string | null;
+  description: string | null;
+  logo?: string | null;
 };
 
 export type LevelMeta = {
@@ -19,4 +20,32 @@ export type LevelData = {
   tx_ref: string;
   customization?: LevelCustomization;
   meta?: LevelMeta;
+};
+
+// Types for transmitted in the callback triggered by PayChangu
+export type LevelAmountSplit = {
+  fee_paid_by_customer: number;
+  fee_paid_by_merchant: number;
+  total_paid_by_customer: number;
+  amount_received_by_merchant: number;
+};
+
+export type LevelCallbackPayload = {
+  event_type: string;
+  first_name: string;
+  email: string;
+  currency: "MWK" | "USD";
+  amount: number;
+  charge: number;
+  amount_split: LevelAmountSplit;
+  total_amount_paid: number;
+  mode: string;
+  type: string;
+  status: string;
+  reference: string;
+  tx_ref: string;
+  customization: LevelCustomization;
+  meta: LevelMeta | null;
+  created_at: string;
+  updated_at: string;
 };
